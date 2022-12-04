8b200c329530284b3a6d2df55c0df7dbc5209dd3
Juan David Moreno Alfonso <jdma253@gmail.com>
2022-12-04 18:29:09 -0500

    cuartoa modificacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,30 @@ import { ArgumentOutOfRangeError, from } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //Datos
   funcion = 'sin(x)';
   iteraciones = 10;
   variable = 'x';
-  guardarFunciones: number[] = [];
+  guardarResultados: number[] = [];
 
-  //añadir función
+  //Guardar resultados en el array
   addFunction(dato: number) {
-    this.guardarFunciones.push(dato);
+    this.guardarResultados.push(dato);
   }
 
+  //Eliminar resultados del array
+  deleteFunction() {
+    this.guardarResultados.splice(0, this.guardarResultados.length);
+  }
+
+  //Eliminar mayúsculas para evitar errores
   deleteUpperCase() {
     this.funcion = this.funcion.toLowerCase();
     this.variable = this.variable.toLowerCase();
-
   }
 
   newtonRaphson() {
+    //Validación de datos
     if (this.iteraciones <= 0 || this.funcion.length == 0 || this.variable.length <= 0 || this.variable.length >= 2) {
       (this.iteraciones <= 0) ?
         alert('iteraciones no puede ser menor o igual a 0') : (this.funcion.length == 0) ?
@@ -33,25 +40,28 @@ export class AppComponent {
               alert('más de un caracter en la variable') : '';
     } else {
       this.deleteUpperCase();
-      let resultador = 1;
+      let resultador = 1;//X0 = 1 para la forumla de newton-raphson
+
+      //Convertir el string en un objeto para poder aplicar los métodos (String to functionNode)
       let f = math.parse(this.funcion);
       let variable = math.parse(this.variable);
       let fx = math.derivative(f, variable);
+
+      //Prepara al objeto para la evaluación de las variables que tenga
       let funcion = f.compile();
       let derivada = fx.compile();
-      var scope = { x: resultador };
+
+      //Se crea un objeto que posee el/los valores para evaluar la función  f(scope) y f'(scope)
+      let scope = { x: resultador };
       this.deleteFunction();
+
       for (let i = 0; i < this.iteraciones; i++) {
-        var resultadorTemporal = parseFloat(resultador.toFixed(10));
+        let resultadorTemporal = parseFloat(resultador.toFixed(10));
         this.addFunction(resultadorTemporal);
         resultador = resultador - (funcion.evaluate(scope) / derivada.evaluate(scope));
         scope.x = resultador;
       }
     }
-  }
-
-  deleteFunction() {
-    this.guardarFunciones.splice(0, this.guardarFunciones.length);
   }
 
 }
